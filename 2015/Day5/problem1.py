6739b8334ead56312a90542d699eea76c85b0428
Antoine Daigneault-Demers <antoine.d.demers@hotmail.com>
2020-12-03 00:22:48 -0500

    2020 day 3

--- 2015/Day5/problem1.py
+++ 2015/Day5/problem1.py
@@ -10,16 +10,20 @@ def main():
     for entry in entries:
         if has_a_double(entry) and has_three_vowels(entry) and has_no_bad_double(entry):
             nice_strings += 1
-    
+        
     print(nice_strings)
 
 def has_no_bad_double(entry):
-    return "ab" not in entry and "bc" not in entry and "pq" not in entry and "xy" not in entry
+    return ("ab" not in entry) and ("cd" not in entry) and ("pq" not in entry) and ("xy" not in entry)
 
 def has_three_vowels(entry):
     vowels_count = 0
     
     for letter in entry:
+        if letter in ['a', 'e', 'i', 'o', 'u']:
+            vowels_count += 1
+
+    return vowels_count >= 3
 
 def has_a_double(entry):
     for i in range(len(entry) - 1):
--- 2015/Day5/problem2.py
+++ 2015/Day5/problem2.py
@@ -5,5 +5,31 @@ def main():
     with open("input.txt") as f:
         entries = f.readlines()
 
+    nice_strings = 0
+
+    for entry in entries:
+        if has_a_repeat(entry) and has_two_pairs(entry):
+            nice_strings += 1
+        
+    print(nice_strings)
+
+def has_two_pairs(entry):
+    pairs = []
+    
+    for i in range(len(entry) - 1):
+    	pair = entry[i:i+1]
+    	if pair in pairs:
+    		return True
+    	else:
+    		pairs.append(pair)
+
+    return False
+
+def has_a_repeat(entry):
+    for i in range(len(entry) - 2):
+        if entry[i] == entry[i + 2]:
+            return True
+    
+    return False
 
 main()
