a407a43e21c2ac2e870ac1ffc4e21b0fc8789ccf
Antoine Daigneault-Demers <antoine.d.demers@hotmail.com>
2020-12-04 12:54:00 -0500

    2020 Day 4

--- Day4/problem1.py
+++ Day4/problem1.py
@@ -1,20 +1,48 @@
+def extract_fields(passport):
+
+	fields_whole = passport[1:].split(' ')
+
+	fields = []
+
+	for field in fields_whole:
+		fields.append(field.split(':')[0])
+
+	return fields
+
+
 def problem1():
 	with open("input1.txt") as f:
-	    trees = f.readlines()
-	trees = [x.strip() for x in trees]
+	    passports = f.readlines()
+	passports = [x.strip() for x in passports]
 
-	x = 0
-	y = 0
-	hits = 0
+	passports_separated = ['']
 
-	while y < len(trees) - 1:
-		x += 3
-		y += 1
-		x = x % len(trees[0])
+	i = 0
+	
+	for x in range(len(passports)):
+		if passports[x] == '':
+			i += 1
+			passports_separated.append('')
+		else:
+			passports_separated[i] = passports_separated[i] + ' ' + passports[x]
 
-		if trees[y][x] == '#':
-			hits += 1
+	valids = 0
 
-	return hits
+	for passport in passports_separated:
+		valid = True
+
+		fields = extract_fields(passport)
+
+		for field in ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']:
+			if field not in fields:
+				print(passport)
+				print(field)
+				valid = False
+				break
+
+		if valid:
+			valids += 1
+
+	return valids
 
 print(problem1())
