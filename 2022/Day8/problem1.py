9f539b0bc627a79f449a7c8b9da65a7cb8ff0d20
Antoine Daigneault-Demers <antoine.d.demers@hotmail.com>
2022-12-08 00:31:28 -0500

    2022 Day 8

--- 2022/Day8/problem1.py
+++ 2022/Day8/problem1.py
@@ -3,7 +3,48 @@ import copy
 
 def main():
     with open("input.txt") as f:
-        entries = f.readlines()
+        lines = f.read().split("\n")
 
+    total_trees = 0
+
+    seen = set()
+    
+    for i in range(len(lines)):
+        curr_tallest = -1
+        for j in range(len(lines[0])):
+            if int(lines[i][j]) > curr_tallest:
+                if (i, j) not in seen:
+                    seen.add((i,j))
+                    total_trees += 1
+                curr_tallest = int(lines[i][j])
+
+    for i in range(len(lines)):
+        curr_tallest = -1
+        for j in range(len(lines[0])-1, 0, -1):
+            if int(lines[i][j]) > curr_tallest:
+                if (i, j) not in seen:
+                    seen.add((i,j))
+                    total_trees += 1
+                curr_tallest = int(lines[i][j])
+
+    for j in range(len(lines[0])):
+        curr_tallest = -1
+        for i in range(len(lines)):
+            if int(lines[i][j]) > curr_tallest:
+                if (i, j) not in seen:
+                    seen.add((i,j))
+                    total_trees += 1
+                curr_tallest = int(lines[i][j])
+
+    for j in range(len(lines[0])):
+        curr_tallest = -1
+        for i in range(len(lines)-1, 0, -1):
+            if int(lines[i][j]) > curr_tallest:
+                if (i, j) not in seen:
+                    seen.add((i,j))
+                    total_trees += 1
+                curr_tallest = int(lines[i][j])
+
+    print(total_trees)
 
 main()
--- 2022/Day8/problem2.py
+++ 2022/Day8/problem2.py
@@ -1,9 +1,74 @@
 import math
 import copy
 
+def calculate_scenic_score_left(grid, i, j):
+    tree_height = grid[i][j]
+    
+    score = 0
+    
+    j -= 1
+    while j >= 0:
+        score += 1
+        if tree_height <= grid[i][j]:
+            break
+        j -= 1
+
+    return score
+
+def calculate_scenic_score_right(grid, i, j):
+    tree_height = grid[i][j]
+    
+    score = 0
+    
+    j += 1
+    while j < len(grid[0]):
+        score += 1
+        if tree_height <= grid[i][j]:
+            break
+        j += 1
+
+    return score
+
+def calculate_scenic_score_top(grid, i, j):
+    tree_height = grid[i][j]
+    
+    score = 0
+    
+    i -= 1
+    while i >= 0:
+        score += 1
+        if tree_height <= grid[i][j]:
+            break
+        i -= 1
+
+    return score
+
+def calculate_scenic_score_bot(grid, i, j):
+    tree_height = grid[i][j]
+    
+    score = 0
+    
+    i += 1
+    while i < len(grid):
+        score += 1
+        if tree_height <= grid[i][j]:
+            break
+        i += 1
+
+    return score
+
 def main():
     with open("input.txt") as f:
-        entries = f.readlines()
+        lines = f.read().split("\n")
 
+    highest_score = 0
+    
+    for i in range(len(lines)):
+        for j in range(len(lines[0])):
+            score = calculate_scenic_score_left(lines, i, j) * calculate_scenic_score_right(lines, i, j) * calculate_scenic_score_top(lines, i, j) * calculate_scenic_score_bot(lines, i, j)
+
+            highest_score = max(highest_score, score)
+
+    print(highest_score)
 
 main()
