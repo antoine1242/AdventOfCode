d891740d9de947816a56eebcf859fa2e1c08349b
Antoine Daigneault-Demers <antoine.d.demers@hotmail.com>
2022-12-10 00:31:25 -0500

    2022 Day 10

--- 2022/Day10/problem1.py
+++ 2022/Day10/problem1.py
@@ -3,7 +3,40 @@ import copy
 
 def main():
     with open("input.txt") as f:
-        entries = f.readlines()
+        lines = f.read().split("\n")
+
+    total = 0
+
+    register = 1
+    cycle = 0
+
+    check = [20, 60, 100, 140, 180, 220]
+
+    for line in lines:
+        op = line.split(" ")[0]
+
+        if op != "noop":
+            num = int(line.split(" ")[1])
+
+        if op == "noop":
+            cycle += 1
+            if cycle in check:
+                total += register * cycle
+
+        else:
+            
+
+            cycle += 1
+            if cycle in check:
+                total += register * cycle
+
+            cycle += 1
+            if cycle in check:
+                total += register * cycle
+
+            register += num
 
 
+    print(total)
+    
 main()
--- 2022/Day10/problem2.py
+++ 2022/Day10/problem2.py
@@ -1,9 +1,44 @@
 import math
 import copy
 
+def draw_pixel(grid, cycle, register):
+    curr_i = (cycle // 40)  # Pas sûr
+    curr_j = (cycle % 40) - 1 # Pas sûr
+
+    if abs(curr_j - register) <= 1:
+        grid[curr_i][curr_j] = "#"
+    else:
+        grid[curr_i][curr_j] = "." 
+
 def main():
     with open("input.txt") as f:
-        entries = f.readlines()
+        lines = f.read().split("\n")
 
+    grid = [["X" for i in range(40)] for j in range(7)]
 
+    register = 1
+    cycle = 0
+
+    for line in lines:
+        op = line.split(" ")[0]
+
+        if op != "noop":
+            num = int(line.split(" ")[1])
+
+        if op == "noop":
+            cycle += 1
+            draw_pixel(grid, cycle, register)
+
+        else:
+            cycle += 1
+            draw_pixel(grid, cycle, register)
+
+            cycle += 1
+            draw_pixel(grid, cycle, register)
+
+            register += num
+
+    for row in grid:
+        print(row)
+    
 main()
