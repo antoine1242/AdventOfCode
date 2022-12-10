159d2e1f3fd0c9fb9c35cf0dc9c03aed5db16f9c
Antoine Daigneault-Demers <antoine.d.demers@hotmail.com>
2022-12-10 11:57:14 -0500

    Clean 2022 Day 1

--- 2022/Day1/problem1.py
+++ 2022/Day1/problem1.py
@@ -5,11 +5,7 @@ def main():
     with open("input.txt") as f:
         groups = f.read().split("\n\n")
 
-    calories_per_elf = [sum([int(value) for value in array]) for array in [group.split("\n") for group in groups]]
-    calories_per_elf.sort()
-    calories_per_elf.reverse()
-    
-    top_1 = calories_per_elf[0]
+    top_1 = sorted([sum([int(value) for value in array]) for array in [group.split("\n") for group in groups]])[::-1][0]
     print(top_1)
 
 main()
--- 2022/Day1/problem2.py
+++ 2022/Day1/problem2.py
@@ -4,12 +4,8 @@ import copy
 def main():
     with open("input.txt") as f:
         groups = f.read().split("\n\n")
-
-    calories_per_elf = [sum([int(value) for value in array]) for array in [group.split("\n") for group in groups]]
-    calories_per_elf.sort()
-    calories_per_elf.reverse()
     
-    sum_top_3 = sum(calories_per_elf[0:3])
-    print(sum_top_3)
+    top3 = sum(sorted([sum([int(value) for value in array]) for array in [group.split("\n") for group in groups]])[::-1][0:3])
+    print(top3)
 
 main()
